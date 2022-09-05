e096b21f15e958277734b3420d2e189d17af5128
MirwaisKhulmi <107170264+MirwaisKhulmi@users.noreply.github.com>
2022-09-05 21:50:21 +0430

    Update Mirwais.py

--- Mirwais.py
+++ Mirwais.py
@@ -342,7 +342,7 @@ def rcrack(uid,pwx,tl):
 
             'upgrade-insecure-requests': '1',
 
-             'user-agent':'Mozilla/5.0 (Linux; Android 7.1.1; Build/LMY47O.H18; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36',}
+             'user-agent':'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16',}
 
             lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
 
